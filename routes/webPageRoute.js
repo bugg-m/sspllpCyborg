3b1c28d31f4479e5a60ed222571fc665c2898150
vivek <krvivi28@gmail.com>
2022-11-03 01:28:02 +0530

    security fixed

--- routes/webPageRoute.js
+++ routes/webPageRoute.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const routerWeb = new express.Router();
 const cookieParser=require("cookie-parser");
+const auth=require("../middleware/auth");
 
 // Get request to go to home page
 routerWeb.get("/", (req, res) => {
   res.render("index");
 });
-routerWeb.get("/productlist", (req, res) => {
-  res.render("productlist");
+// Get request to go to home page
+routerWeb.get("/productlist", auth , (req, res) => {
+  res.render("productlistform");
 });
 
+
 // Get request for dev test
-routerWeb.get("/test", (req, res) => {
+routerWeb.get("/test", auth, (req, res) => {
   res.render("test");
 });
 routerWeb.get("/importlogin", (req, res) => {
@@ -22,40 +25,56 @@ routerWeb.get("/profile", (req, res) => {
   res.render("profile");
 });
 
-routerWeb.get("/exim", (req, res) => {
-  res.render("client");
+routerWeb.get("/export",auth, (req, res) => {
+  res.render("exim");
 });
-routerWeb.get("/sell", (req, res) => {
-  res.render("productlist");
+routerWeb.get("/import",auth, (req, res) => {
+  res.render("importuserdata");
 });
-routerWeb.get("/product", (req, res) => {
-  res.render("marketplace");
+
+routerWeb.get("/mktplace", (req, res) => {
+  res.render("mktplace");
 });
-routerWeb.get("/vision", (req, res) => {
-  res.render("vision");
+routerWeb.get("/logout", auth, async (req, res) => {
+  try {
+
+    // for single logout
+    // req.user.tokens=req.user.tokens.filter((ele)=>
+    // {
+    //   return ele.token !== req.token
+
+    // })
+
+    // to logout from all devices 
+    req.user.tokens=[];
+
+
+
+    res.clearCookie("jwt");
+    console.log("logout successful");
+    await req.user.save();
+    res.render("reglog");
+    
+  } catch (error) {
+    res.status(500).send(error);
+    
+  }
 });
 routerWeb.get("/contact", (req, res) => {
   // console.log(`this is our ${req.cookies.jwt}`);
   res.render("contact");
 });
-routerWeb.get("/register", (req, res) => {
-  res.render("register");
-});
-routerWeb.get("/blog", (req, res) => {
-  res.render("blog");
-});
+
 routerWeb.get("/reglog", (req, res) => {
   res.render("reglog");
 });
 // routerWeb.get("/ourteam", (req, res) => {
 //   res.render("ourteam");
 // });
-routerWeb.get("/upgrade", (req, res) => {
+routerWeb.get("/upgrade", auth, (req, res) => {
   res.render("plans");
 });
-routerWeb.get("/planpay", (req, res) => {
-  res.render("planpay");
-});
+
 
 routerWeb.get("*", (req, res) => {
   res.status(404).render("404", { err: "page not found" });
